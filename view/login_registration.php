734a451b68798f32745e38af5b80429aed63676b
labionabazi <31927643+labionabazi@users.noreply.github.com>
2018-03-22 11:06:31 +0100

    Registration View added

--- view/login_registration.php
+++ view/login_registration.php
@@ -1,7 +1,23 @@
 <?php
-  /**
-   * Registratons-Formular
-   * Das Formular wird mithilfe des Formulargenerators erstellt.
-   */
- 
+/**
+ * Registratons-Formular
+ * Das Formular wird mithilfe des Formulargenerators erstellt.
+ */
+/**
+ * Registration-Formular
+ * Das Formular wird mithilfe des Formulargenerators erstellt.
+ */
+$lblClass = "col-md-2";
+$eltClass = "col-md-4";
+$btnClass = "btn btn-success";
+$form = new Form($GLOBALS['appurl'] . "/registration");
+$button = new ButtonBuilder();
+echo $form->input()->label('Firstname')->name('firstname')->type('text')->lblClass($lblClass)->eltClass($eltClass);
+echo $form->input()->label('Surename')->name('surename')->type('text')->lblClass($lblClass)->eltClass($eltClass);
+echo $form->input()->label('E-Mail')->name('email')->type('text')->lblClass($lblClass)->eltClass($eltClass);
+echo $form->input()->label('Passwort')->name('passwort')->type('text')->lblClass($lblClass)->eltClass($eltClass);
+echo $button->start($lblClass, $eltClass);
+echo $button->label('Register')->name('send')->type('submit')->class('btn-success');
+echo $button->end();
+echo $form->end();
 ?>
