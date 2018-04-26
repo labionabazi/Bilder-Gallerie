b62e7796afc414c106a9ed8eee72d08d01f521a8
taabrde2 <31621723+taabrde2@users.noreply.github.com>
2018-04-26 10:26:43 +0200

    Changed gitignore file

---
+++ controller/GallerieController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vmadmin
+ * Date: 29.03.2018
+ * Time: 10:16
+ */
+
+class GallerieController
+{
+
+}
---
+++ view/pri_gallerie_home.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vmadmin
+ * Date: 29.03.2018
+ * Time: 10:17
+ */
