d56d33c3ea2070e0203adb8707b5d7838473f6a8
Labion Abazi <31927643+labionabazi@users.noreply.github.com>
2018-05-31 11:17:43 +0200

    Labion isch peer

--- view/pri_gallerie_home.php
+++ view/pri_gallerie_home.php
@@ -1,5 +1,28 @@
 <?php
-echo '<button class="btn btn-info" style="margin: 0 0 5px 0">Add Gallerie</button>';
+echo '  <button id="addGallerie" class="btn btn-info">Add Gallerie</button>
+        <form class="form-horizontal gallerieForm hidden" action="' . $GLOBALS['appurl'] . '/gallerie/createGallerie" method="POST">
+            <div class="component" data-html="true">
+                    <div class="form-group">
+                        <label class="col-md-2 control-label" for="textinput">Name</label>
+                    <div class="col-md-4">
+                        <input name="name" type="text" value="" class="form-control">
+                    </div>
+                    </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label" for="textinput">Description</label>
+                    <div class="col-md-4">
+                        <input name="description" type="text" value="" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2" control-label\'="" for="textinput">&nbsp;</label>
+                    <div class="col-md-4">
+                        <input name="send" id="submitGallerie" type="submit" class="btn btn-success" value="Create">
+                    </div>
+                </div>
+            </div>
+        </form>';
+
 for($i = 0; $i < count($gallerie); $i++){
     echo '<div class="card">';
     echo '<img src="" style="width:100%">';
@@ -10,6 +33,3 @@ for($i = 0; $i < count($gallerie); $i++){
     echo '</div></div>';
 }
 ?>
-
-
-
