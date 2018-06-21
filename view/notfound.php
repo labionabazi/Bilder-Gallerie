3f71e12de92fab1f89c0996cf7350a1dcb1191fb
Labion Abazi <31927643+labionabazi@users.noreply.github.com>
2018-06-21 10:24:49 +0200

    last last last laaaaast ix
    last fix hs

--- view/notfound.php
+++ view/notfound.php
@@ -1,2 +1,9 @@
 
-<h1>Seite nicht gefunden</h1>
+<h1>Seite nicht gefunden... </h1>
+<?php
+if(isset($_SESSION['uid'])){
+    echo '<a href='.$GLOBALS['appurl'].'/gallerie/home'.' class="btn btn-success">Go to Home</a>';
+}else{
+    echo '<a href='.$GLOBALS['appurl'].'/login/login'.' class="btn btn-success">Go to Login</a>';
+}
+?>
